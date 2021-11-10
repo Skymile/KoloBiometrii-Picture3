054b03ccad94ebc29e389a8a4c267f9ae6e4b0bb
Patryk Milewski <mpatryk97@gmail.com>
2021-11-10 05:30:41 +0100

    Add project files.

---
+++ Gradient.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace WpfApp5
+{
+	public class Gradient
+	{
+		public static System.Windows.Media.Imaging.BitmapSource ToSource(Bitmap bmp)
+		{
+			var data = bmp.LockBits(
+				new Rectangle(Point.Empty, bmp.Size),
+				ImageLockMode.ReadWrite,
+				PixelFormat.Format24bppRgb
+			);
+
+			var bmpSource = System.Windows.Media.Imaging.BitmapSource.Create(
+				data.Width,
+				data.Height,
+				96, 96,
+				System.Windows.Media.PixelFormats.Bgr24,
+				null,
+				data.Scan0,
+				data.Stride * data.Height,
+				data.Stride
+			);
+
+			bmp.UnlockBits(data);
+			return bmpSource;
+		}
+	}
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Drawing;
+using System.Windows;
+
+namespace WpfApp5
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			var bmp = new Bitmap(
+				"C:/Users/mpatr/Desktop/apple/apple_noise.png"
+			//256, 256
+			);
+
+			int ratioX = bmp.Width / 256;
+			int ratioY = bmp.Height / 256;
+
+			for (int i = 0; i < 256; i++)
+				for (int j = 0; j < 256; j++)
+					for (int x = 0; x < ratioX; x++)
+						for (int y = 0; y < ratioY; y++)
+						{
+							int c = j;
+							if (c > 255)
+								c = 255;
+
+							var color = bmp.GetPixel(
+								i * ratioX + x,
+								j * ratioY + y
+							);
+
+							bmp.SetPixel(
+								i * ratioX + x,
+								j * ratioY + y,
+								Color.FromArgb(
+									color.R / 2,
+									(color.G + c) / 2,
+									(color.B + i) / 2
+								)
+							);
+						}
+
+			MainImage.Source = Gradient.ToSource(bmp);
+		}
+	}
+}
